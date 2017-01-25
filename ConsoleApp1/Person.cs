8aba1c854694e0f9e36a1dce5edf20bb11891844
Omar Jbara <omar.Jbara2@gmail.com>
2017-01-24 22:23:37 -0500

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => FirstName + " " + LastName;
+
+        public Person(int age, string firstName, string lastName)
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public bool IsMajeur()
+        {
+            return Age > 18;
+        }
+
+        public int? LengthName()
+        {
+            return FirstName?.Length + LastName?.Length;
+        }
+
+        public override string ToString()
+        {
+            return $"{FullName} a {Age} ans.\nIl { (IsMajeur() ? "est" : "n'est pas") } majeur";
+        }
+    }
+}
